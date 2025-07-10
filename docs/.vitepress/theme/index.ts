0efe22eaf735ba420d42c55ffb20a316c20b53ce
wangkc <wkclz@qq.com>
2025-07-11 00:52:10 +0800

    调整 css, 扩大内容区域

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,8 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import './css/style.css'
+import './css/custom.css'
 
 export default {
   extends: DefaultTheme,
